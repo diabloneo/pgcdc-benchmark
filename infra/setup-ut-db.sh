a1fc0c8aa83a11a13725d642ea06122e5ef25fdb
diabloneo <diabloneo@gmail.com>
2021-06-08 14:32:44 +0800

    Add tools to setup test Postgres server
    Signed-off-by: diabloneo <diabloneo@gmail.com>

---
+++ infra/setup-ut-db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+BASE_PATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+echo ${BASE_PATH}
+
+image="postgres:13.2"
+# echo "Pull docker image"
+# docker pull ${image}
+container=data-test-pg
+echo "Stop docker container ${container}"
+docker rm -v -f ${container}
+echo "Start docker container ${container}"
+
+docker run -d -p 127.0.0.1:5432:5432 --name ${container} \
+	-v ${BASE_PATH}/postgres/postgresql.conf:/etc/postgresql/postgresql.conf \
+	-e POSTGRES_PASSWORD=password \
+	${image} \
+	-c 'config_file=/etc/postgresql/postgresql.conf'
+
+echo "Wait postgres to start"
+sleep 10
+
+echo "Create database"
+docker exec -it -u postgres ${container} psql -c 'CREATE DATABASE "data" OWNER=postgres;'
+
+echo "Done"
